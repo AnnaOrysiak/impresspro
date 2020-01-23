167d3d1f1057638455ac4b3cbd560df6ab79457f
AnnaOrysiak <walachan88@gmail.com>
2020-01-23 14:43:57 +0100

    small refactor

--- scripts/script.js
+++ scripts/script.js
@@ -2,21 +2,20 @@ const mainScripts = () => {
   const btn = document.querySelector(".button-main");
   const icon = document.querySelector(".button-main__icon");
   const faqBoxes = document.querySelectorAll('.faq-box');
-  const faqHeaders = document.querySelectorAll(".faq-box__header");
 
   function handleAccordionAnswer() {
     const parentBox = this.parentElement;
 
     if (parentBox.classList.contains('active')) return;
     else {
-      faqBoxes.forEach((item, i) => item.classList.remove('active'))
-      parentBox.classList.add('active');
+      faqBoxes.forEach((item, i) => item.classList.remove('active'));
+      parentBox.classList.add('active');;
     }
   }
 
   btn.addEventListener("mouseover", () => icon.classList.add("active"));
   btn.addEventListener("mouseout", () => icon.classList.remove("active"));
-  for (const item of faqHeaders) item.addEventListener('click', handleAccordionAnswer);
+  for (const item of document.querySelectorAll(".faq-box__header")) { item.addEventListener('click', handleAccordionAnswer) };
 }
 
 const runMainScripts = mainScripts;
