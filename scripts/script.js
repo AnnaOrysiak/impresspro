294b7cdb06843426d03361db4da05b1e0fdcfbb4
AnnaOrysiak <walachan88@gmail.com>
2020-01-23 14:35:06 +0100

    added some corrections

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,23 @@
 const mainScripts = () => {
-
   const btn = document.querySelector(".button-main");
   const icon = document.querySelector(".button-main__icon");
   const faqBoxes = document.querySelectorAll('.faq-box');
   const faqHeaders = document.querySelectorAll(".faq-box__header");
 
-  const addIconClass = () => {
-    icon.classList.add("active");
-  }
-
-  const removeIconClass = () => {
-    icon.classList.remove("active");
-  }
-
-  function handleAccordionAnswer(e) {
+  function handleAccordionAnswer() {
     const parentBox = this.parentElement;
 
-    if (parentBox.classList.contains('active')) {
-      return;
-    }
+    if (parentBox.classList.contains('active')) return;
     else {
-      faqBoxes.forEach((item, i) => {
-        item.classList.remove('active');
-      })
+      faqBoxes.forEach((item, i) => item.classList.remove('active'))
       parentBox.classList.add('active');
     }
   }
 
-  btn.addEventListener("mouseover", addIconClass);
-  btn.addEventListener("mouseout", removeIconClass);
-  for (const item of faqHeaders) {
-    item.addEventListener('click', handleAccordionAnswer);
-  }
+  btn.addEventListener("mouseover", () => icon.classList.add("active"));
+  btn.addEventListener("mouseout", () => icon.classList.remove("active"));
+  for (const item of faqHeaders) item.addEventListener('click', handleAccordionAnswer);
 }
 
-const runMainScripts = mainScripts();
-
+const runMainScripts = mainScripts;
 runMainScripts();
